218dc0b4afe371a7d1ef1b00ddfc9784d291c5f9
Harsh-choudhary1998 <harshchoudhary.mbd@gmail.com>
2024-11-28 17:10:30 +0000

    variables-commit

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 134433
+let accountEmail = "harsh@google.com"
+var accountPassword = "abc@123"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+
+/*
+prefer not to use var as it has a scope issue
+
+*/
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity])
